05dd7e3a021545b2c23941fd76bbc1934bb92633
Oz996 <ozgun996@hotmail.com>
2023-06-17 21:30:06 +0200

    Added preview of post body

--- frontend/src/Pages/Blogs.jsx
+++ frontend/src/Pages/Blogs.jsx
@@ -13,6 +13,7 @@ const Blogs = () => {
   useEffect(() => {
     getPosts();
   });
+
   return (
     <div className="container d-flex flex-column mb-3 w-50">
       <h1 className="my-5 text-center">Latest Blogs</h1>
@@ -31,6 +32,8 @@ const Blogs = () => {
             className="border rounded-3 p-3 my-3 w-"
           >
             <h3>{post.title}</h3>
+            {post.body.length > 80 ? <p className="my-3 fs-5">{post.body.substring(0,80)}...</p>
+            : <p className="my-3 fs-5">{post.body}</p>}
             <h6>{`By: ${post.user.email}`}</h6>
             <p>{`Created at: ${post.createdAt}`}</p>
           </div>
